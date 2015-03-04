8d3fe142cc6048ee8893f6e472734e598e3b2f44
cache-_s <cache-_s@epitech.eu>
2015-03-04 09:12:25 +0100

    yolo

--- sources/args.c
+++ sources/args.c
@@ -5,7 +5,7 @@
 ** Login   <cache-_s@epitech.net>
 ** 
 ** Started on  Tue Mar  3 19:08:40 2015 Sebastien Cache-Delanos
-** Last update Tue Mar  3 20:28:46 2015 Sebastien Cache-Delanos
+** Last update Wed Mar  4 09:09:52 2015 Sebastien Cache-Delanos
 */
 
 #include			"lemipc.h"
@@ -18,6 +18,24 @@ int				pickArmy()
 
 int				getArmy(char* arg)
 {
-  (void)arg;
-  return (0);
+  char*				tmp;
+  int				army;
+
+  tmp = malloc(1 * sizeof(*tmp));
+  if (tmp == NULL)
+    {
+      printf("Malloc fail in getArmy()");
+      return (-1);
+    }
+  if (strncmp(arg, "--army=", 7) == 0 && strlen(arg) == 8)
+    {
+      strncpy(tmp, arg + 7, 1);
+      army = atoi(tmp);
+      free(tmp);
+      if (army >= 1 && army <= 5)
+	return (army);
+    }
+  printf("Invalid argument\n");
+  free(tmp);
+  return (-1);
 }
